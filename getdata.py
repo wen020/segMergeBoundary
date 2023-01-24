a7a4eb6ef42c076660bd8140c6bdc9d9daa719bf
wen <wen@8glabs.com>
2023-01-24 16:57:16 +0000

    fix

--- getdata.py
+++ getdata.py
@@ -27,30 +27,39 @@ train_data = ["top_mosaic_09cm_area1",
              "top_mosaic_09cm_area37"]
 train_imgs_output_dir = "./data/train/images"
 train_masks_output_dir = "./data/train/masks"
+train_boundarys_output_dir = "./data/train/boundarys"
 val_imgs_output_dir = "./data/val/images"
 val_masks_output_dir = "./data/val/masks"
+val_boundarys_output_dir = "./data/val/boundarys"
 test_imgs_output_dir = "./data/test/images"
 test_masks_output_dir = "./data/test/masks"
+test_boundarys_output_dir = "./data/test/boundarys"
 if not os.path.exists(train_imgs_output_dir):
     os.makedirs(train_imgs_output_dir)
+if not os.path.exists(train_boundarys_output_dir):
+    os.makedirs(train_boundarys_output_dir)
 if not os.path.exists(train_masks_output_dir):
     os.makedirs(train_masks_output_dir)
 if not os.path.exists(val_imgs_output_dir):
     os.makedirs(val_imgs_output_dir)
+if not os.path.exists(val_boundarys_output_dir):
+    os.makedirs(val_boundarys_output_dir)
 if not os.path.exists(val_masks_output_dir):
     os.makedirs(val_masks_output_dir)
 if not os.path.exists(test_imgs_output_dir):
     os.makedirs(test_imgs_output_dir)
 if not os.path.exists(test_masks_output_dir):
     os.makedirs(test_masks_output_dir)
+if not os.path.exists(test_boundarys_output_dir):
+    os.makedirs(test_boundarys_output_dir)
 
 
 imgs_dir="./data/images"
 masks_dir="./data/masks"
 boundarys_dir="./data/boundarys"
-img_paths = glob.glob(os.path.join(imgs_dir, "*.tif"))
-mask_paths = glob.glob(os.path.join(masks_dir, "*.tif"))
-boundary_paths = glob.glob(os.path.join(boundarys_dir, "*.tif"))
+img_paths = sorted(glob.glob(os.path.join(imgs_dir, "*.tif")))
+mask_paths = sorted(glob.glob(os.path.join(masks_dir, "*.tif")))
+boundary_paths = sorted(glob.glob(os.path.join(boundarys_dir, "*.tif")))
 print(img_paths)
 print(mask_paths)
 print(boundary_paths)
@@ -64,8 +73,8 @@ Clutter = np.array([0, 0, 255]) # label 5
 Boundary = np.array([0, 0, 0]) # label 6
 num_classes = 6
 
-def get_img_mask_padded(image, mask, patch_size, mode):
-    img, mask = np.array(image), np.array(mask)
+def get_img_mask_padded(image, mask, boundary, patch_size, mode):
+    img, mask, boundary = np.array(image), np.array(mask), np.array(boundary)
     oh, ow = img.shape[0], img.shape[1]
     rh, rw = oh % patch_size, ow % patch_size
     width_pad = 0 if rw == 0 else patch_size - rw
@@ -79,10 +88,13 @@ def get_img_mask_padded(image, mask, patch_size, mode):
 
     pad_mask = albu.PadIfNeeded(min_height=h, min_width=w, position='bottom_right',
                                 border_mode=0, value=[0, 0, 0])(image=mask)
-    img_pad, mask_pad = pad_img['image'], pad_mask['image']
+    pad_boundary = albu.PadIfNeeded(min_height=h, min_width=w, position='bottom_right',
+                            border_mode=0, value=[0, 0, 0])(image=boundary)
+    img_pad, mask_pad, boundary_pad = pad_img['image'], pad_mask['image'], pad_boundary['image']
     img_pad = cv2.cvtColor(np.array(img_pad), cv2.COLOR_RGB2BGR)
     mask_pad = cv2.cvtColor(np.array(mask_pad), cv2.COLOR_RGB2BGR)
-    return img_pad, mask_pad
+    boundary_pad = cv2.cvtColor(np.array(boundary_pad), cv2.COLOR_RGB2BGR)
+    return img_pad, mask_pad, boundary_pad
 
 
 def pv2rgb(mask):
@@ -105,23 +117,27 @@ def car_color_replace(mask):
     return mask
 
 
-def rgb_to_2D_label(_label):
-    print(_label.size)
-    _label = _label.transpose(2, 0, 1)
-    label_seg = np.zeros(_label.shape[1:], dtype=np.uint8)
-    label_seg[np.all(_label.transpose([1, 2, 0]) == ImSurf, axis=-1)] = 0
-    label_seg[np.all(_label.transpose([1, 2, 0]) == Building, axis=-1)] = 1
-    label_seg[np.all(_label.transpose([1, 2, 0]) == LowVeg, axis=-1)] = 2
-    label_seg[np.all(_label.transpose([1, 2, 0]) == Tree, axis=-1)] = 3
-    label_seg[np.all(_label.transpose([1, 2, 0]) == Car, axis=-1)] = 4
-    label_seg[np.all(_label.transpose([1, 2, 0]) == Clutter, axis=-1)] = 5
-    # label_seg[np.all(_label.transpose([1, 2, 0]) == Boundary, axis=-1)] = 6
-    return label_seg
+def rgb_to_2D_label(_mask, _boundary):
+    print(_mask.size)
+    _mask = _mask.transpose(2, 0, 1)
+    mask_seg = np.zeros(_mask.shape[1:], dtype=np.uint8)
+    mask_seg[np.all(_mask.transpose([1, 2, 0]) == ImSurf, axis=-1)] = 0
+    mask_seg[np.all(_mask.transpose([1, 2, 0]) == Building, axis=-1)] = 1
+    mask_seg[np.all(_mask.transpose([1, 2, 0]) == LowVeg, axis=-1)] = 2
+    mask_seg[np.all(_mask.transpose([1, 2, 0]) == Tree, axis=-1)] = 3
+    mask_seg[np.all(_mask.transpose([1, 2, 0]) == Car, axis=-1)] = 4
+    mask_seg[np.all(_mask.transpose([1, 2, 0]) == Clutter, axis=-1)] = 5
+
+    _boundary = _boundary.transpose(2, 0, 1)
+    boundary_seg = np.zeros(_boundary.shape[1:], dtype=np.uint8)
+    boundary_seg[np.all(_boundary.transpose([1, 2, 0]) == Boundary, axis=-1)] = 1
+    return mask_seg, boundary_seg
 
 
-def image_augment(image, mask, patch_size, mode='train', val_scale=1.0):
+def image_augment(image, mask, boundary, patch_size, mode='train', val_scale=1.0):
     image_list = []
     mask_list = []
+    boundary_list = []
     image_width, image_height = image.size[1], image.size[0]
     mask_width, mask_height = mask.size[1], mask.size[0]
     assert image_height == mask_height and image_width == mask_width
@@ -145,65 +161,72 @@ def image_augment(image, mask, patch_size, mode='train', val_scale=1.0):
         # crop_1 = RandomCrop(size=(int(image_width*0.75), int(image_height*0.75)))
         # crop_2 = RandomCrop(size=(int(image_width * 0.5), int(image_height * 0.5)))
         # color = torchvision.transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2)
-        image_h_vlip, mask_h_vlip = h_vlip(image.copy()), h_vlip(mask.copy())
-        image_v_vlip, mask_v_vlip = v_vlip(image.copy()), v_vlip(mask.copy())
+        image_h_vlip, mask_h_vlip, boundary_h_vlip = h_vlip(image.copy()), h_vlip(mask.copy()), h_vlip(boundary.copy())
+        image_v_vlip, mask_v_vlip, boundary_v_vlip = v_vlip(image.copy()), v_vlip(mask.copy()), v_vlip(boundary.copy())
         # image_crop_1, mask_crop_1 = crop_1(image.copy()), crop_1(mask.copy())
         # image_crop_2, mask_crop_2 = crop_2(image.copy()), crop_2(mask.copy())
         # image_color = color(image.copy())
 
         image_list_train = [image, image_h_vlip, image_v_vlip]
         mask_list_train = [mask, mask_h_vlip, mask_v_vlip]
+        boundary_list_train = [boundary, boundary_h_vlip, boundary_v_vlip]
         # image_list_train = [image]
         # mask_list_train = [mask]
         for i in range(len(image_list_train)):
-            image_tmp, mask_tmp = get_img_mask_padded(image_list_train[i], mask_list_train[i], patch_size, mode)
-            mask_tmp = rgb_to_2D_label(mask_tmp.copy())
+            image_tmp, mask_tmp, boundary_tmp = get_img_mask_padded(image_list_train[i], mask_list_train[i], boundary_list_train[i], patch_size, mode)
+            mask_tmp, boundary_tmp = rgb_to_2D_label(mask_tmp.copy(), boundary_tmp.copy())
             image_list.append(image_tmp)
             mask_list.append(mask_tmp)
+            boundary_list.append(boundary_tmp)
     else:
         rescale = Resize(size=(int(image_width * val_scale), int(image_height * val_scale)))
-        image, mask = rescale(image.copy()), rescale(mask.copy())
-        image, mask = get_img_mask_padded(image.copy(), mask.copy(), patch_size, mode)
-        mask = rgb_to_2D_label(mask.copy())
+        image, mask, boundary = rescale(image.copy()), rescale(mask.copy()), rescale(boundary.copy())
+        image, mask, boundary = get_img_mask_padded(image.copy(), mask.copy(), boundary.copy(), patch_size, mode)
+        mask, boundary = rgb_to_2D_label(mask.copy(), boundary.copy())
         image_list.append(image)
         mask_list.append(mask)
-    return image_list, mask_list
+        boundary_list.append(boundary)
+    return image_list, mask_list, boundary_list
 
 
-def randomsizedcrop(image, mask):
+def randomsizedcrop(image, mask, boundary):
     # assert image.shape[:2] == mask.shape
     h, w = image.shape[0], image.shape[1]
-    crop = albu.RandomSizedCrop(min_max_height=(int(3*h//8), int(h//2)), width=h, height=w)(image=image.copy(), mask=mask.copy())
-    img_crop, mask_crop = crop['image'], crop['mask']
-    return img_crop, mask_crop
+    crop = albu.RandomSizedCrop(min_max_height=(int(3*h//8), int(h//2)), width=h, height=w)(image=image.copy(), mask=mask.copy(), boundary=boundary.copy())
+    img_crop, mask_crop, boundary_crop = crop['image'], crop['mask'], crop['boundary']
+    return img_crop, mask_crop, boundary_crop
 
 
-def car_aug(image, mask):
+def car_aug(image, mask, boundary):
     assert image.shape[:2] == mask.shape
-    v_flip = albu.VerticalFlip(p=1.0)(image=image.copy(), mask=mask.copy())
-    h_flip = albu.HorizontalFlip(p=1.0)(image=image.copy(), mask=mask.copy())
-    rotate_90 = albu.RandomRotate90(p=1.0)(image=image.copy(), mask=mask.copy())
+    v_flip = albu.VerticalFlip(p=1.0)(image=image.copy(), mask=mask.copy(), boundary=boundary.copy())
+    h_flip = albu.HorizontalFlip(p=1.0)(image=image.copy(), mask=mask.copy(), boundary=boundary.copy())
+    rotate_90 = albu.RandomRotate90(p=1.0)(image=image.copy(), mask=mask.copy(), boundary=boundary.copy())
     # blur = albu.GaussianBlur(p=1.0)(image=image.copy())
-    image_vflip, mask_vflip = v_flip['image'], v_flip['mask']
-    image_hflip, mask_hflip = h_flip['image'], h_flip['mask']
-    image_rotate, mask_rotate = rotate_90['image'], rotate_90['mask']
+    image_vflip, mask_vflip, boundary_vflip = v_flip['image'], v_flip['mask'], v_flip['boundary']
+    image_hflip, mask_hflip, boundary_hflip = h_flip['image'], h_flip['mask'], h_flip['boundary']
+    image_rotate, mask_rotate, boundary_rotate = rotate_90['image'], rotate_90['mask'], rotate_90['boundary']
     # blur_image = blur['image']
     image_list = [image, image_vflip, image_hflip, image_rotate]
     mask_list = [mask, mask_vflip, mask_hflip, mask_rotate]
+    boundary_list = [boundary, boundary_vflip, boundary_hflip, boundary_rotate]
 
-    return image_list, mask_list
+    return image_list, mask_list, boundary_list
 
 def vaihingen_format(inp):
-    (img_path, mask_path, imgs_output_dir, masks_output_dir, eroded, gt, mode, val_scale, split_size, stride) = inp
+    (img_path, mask_path, boundary_path, imgs_output_dir, masks_output_dir, boundarys_output_dir, eroded, gt, mode, val_scale, split_size, stride) = inp
     img_filename = os.path.splitext(os.path.basename(img_path))[0]
     mask_filename = os.path.splitext(os.path.basename(mask_path))[0]
+    boundary_filename = os.path.splitext(os.path.basename(boundary_path))[0]
     if eroded:
         mask_path = mask_path[:-4] + '_noBoundary.tif'
     img = Image.open(img_path).convert('RGB')
     mask = Image.open(mask_path).convert('RGB')
-    
-    image_pad, mask_pad = get_img_mask_padded(img.copy(), mask.copy(), 16, "train")
-    mask_tmp = rgb_to_2D_label(mask_pad.copy())
+    boundary = Image.open(boundary_path).convert('RGB')
+    # print(img.shape)
+    # print(boundary.shape)
+    image_pad, mask_pad, boundary_pad = get_img_mask_padded(img.copy(), mask.copy(), boundary.copy(), 16, "train")
+    mask_tmp, boundary_tmp = rgb_to_2D_label(mask_pad.copy(),boundary_pad.copy())
     out_mask_path = os.path.join(test_masks_output_dir,
                                  "{}.png".format(mask_filename))
     cv2.imwrite(out_mask_path, mask_tmp)
@@ -218,13 +241,14 @@ def vaihingen_format(inp):
     # print(img_path)
     # print(img.size, mask.size)
     # img and mask shape: WxHxC
-    image_list, mask_list = image_augment(image=img.copy(), mask=mask.copy(), patch_size=split_size,
+    image_list, mask_list, boundary_list = image_augment(image=img.copy(), mask=mask.copy(), boundary=boundary.copy(), patch_size=split_size,
                                           mode=mode, val_scale=val_scale)
     assert img_filename == mask_filename and len(image_list) == len(mask_list)
     for m in range(len(image_list)):
         k = 0
         img = image_list[m]
         mask = mask_list[m]
+        boundary = boundary_list[m]
         assert img.shape[0] == mask.shape[0] and img.shape[1] == mask.shape[1]
         if gt:
             mask = pv2rgb(mask)
@@ -233,15 +257,16 @@ def vaihingen_format(inp):
             for x in range(0, img.shape[1], stride):
                 img_tile = img[y:y + split_size, x:x + split_size]
                 mask_tile = mask[y:y + split_size, x:x + split_size]
+                boundary_tile = boundary[y:y + split_size, x:x + split_size]
 
                 if img_tile.shape[0] == split_size and img_tile.shape[1] == split_size \
                         and mask_tile.shape[0] == split_size and mask_tile.shape[1] == split_size:
-                    image_crop, mask_crop = randomsizedcrop(img_tile, mask_tile)
+                    image_crop, mask_crop, boundary_crop = randomsizedcrop(img_tile, mask_tile, boundary_tile)
                     bins = np.array(range(num_classes + 1))
                     class_pixel_counts, _ = np.histogram(mask_crop, bins=bins)
                     cf = class_pixel_counts / (mask_crop.shape[0] * mask_crop.shape[1])
                     if cf[4] > 0.1 and mode == 'train':
-                        car_imgs, car_masks = car_aug(image_crop, mask_crop)
+                        car_imgs, car_masks, car_boundarys = car_aug(image_crop, mask_crop, boundary_crop)
                         for i in range(len(car_imgs)):
                             out_img_path = os.path.join(imgs_output_dir,
                                                         "{}_{}_{}_{}.tif".format(img_filename, m, k, i))
@@ -251,6 +276,10 @@ def vaihingen_format(inp):
                             out_mask_path = os.path.join(masks_output_dir,
                                                          "{}_{}_{}_{}.png".format(mask_filename, m, k, i))
                             cv2.imwrite(out_mask_path, car_masks[i])
+
+                            out_boundary_path = os.path.join(boundarys_output_dir,
+                                                         "{}_{}_{}_{}.png".format(boundary_filename, m, k, i))
+                            cv2.imwrite(out_boundary_path, car_boundarys[i])
                     else:
                         out_img_path = os.path.join(imgs_output_dir, "{}_{}_{}.tif".format(img_filename, m, k))
                         
@@ -259,15 +288,19 @@ def vaihingen_format(inp):
                         out_mask_path = os.path.join(masks_output_dir, "{}_{}_{}.png".format(mask_filename, m, k))
                         cv2.imwrite(out_mask_path, mask_tile)
 
+                        out_boundary_path = os.path.join(boundarys_output_dir, "{}_{}_{}.png".format(boundary_filename, m, k))
+                        cv2.imwrite(out_boundary_path, boundary_tile)
+
                 k += 1
 
 
 
-for img_path, mask_path in zip(img_paths, mask_paths):
-    print(img_path, mask_path)
+for img_path, mask_path, boundary_path in zip(img_paths, mask_paths, boundary_paths):
     img_filename = os.path.splitext(os.path.basename(img_path))[0]
     print(img_filename)
     mask_filename = os.path.splitext(os.path.basename(mask_path))[0]
+    boundary_path = os.path.join(boundarys_dir, img_filename + '_noBoundary.tif')
+    print(img_path, mask_path, boundary_path)
     # mask_path = mask_path[:-4] + '_noBoundary.tif'
     # img = Image.open(img_path).convert('RGB').resize((1900,2500))
     # mask = Image.open(mask_path).convert('RGB').resize((1900,2500))
@@ -275,10 +308,10 @@ for img_path, mask_path in zip(img_paths, mask_paths):
     # img.show()
     # mask = rgb_to_2D_label(mask.copy())
     if img_filename in train_data:
-        inp = (img_path, mask_path, train_imgs_output_dir, train_masks_output_dir, False, False, "train", 1.0, 1024, 512)
+        inp = (img_path, mask_path, boundary_path, train_imgs_output_dir, train_masks_output_dir, train_boundarys_output_dir, False, False, "train", 1.0, 1024, 512)
         vaihingen_format(inp)
     else:
-        inp = (img_path, mask_path, val_imgs_output_dir, val_masks_output_dir, False, False, "val", 1.0, 1024, 1024)
+        inp = (img_path, mask_path, boundary_path, val_imgs_output_dir, val_masks_output_dir, val_boundarys_output_dir, False, False, "val", 1.0, 1024, 1024)
         vaihingen_format(inp)
         
     # else:
